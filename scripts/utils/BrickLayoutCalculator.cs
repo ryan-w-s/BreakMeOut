6f2fae3d68246a637367fd951dc5e2806244a46e
Ryan Stolliker <ryan.stolliker@gmail.com>
2026-01-31 19:38:58 -0800

    fix(build): Restore missing columns variable in calculator

--- scripts/utils/BrickLayoutCalculator.cs
+++ scripts/utils/BrickLayoutCalculator.cs
@@ -8,7 +8,13 @@ namespace BreakMeOut.Scripts.Utils
         public static (int Columns, int BrickWidth, float MarginX) CalculateLayout(int screenWidth, int spacing)
         {
             // Target width 40px.
+            // Constraint: Margin >= BrickWidth on both sides.
             int brickWidth = 40;
+            
+            // Equation: ScreenWidth >= N * Width + (N - 1) * Spacing + 2 * Width
+            // N <= (1280 - 5 - 80) / (40 + 5) = 1195 / 45 = 26.55
+            
+            int columns = 26;
             
             float contentWidth = (columns * brickWidth) + ((columns - 1) * spacing);
             float marginX = (screenWidth - contentWidth) / 2f;
